862e37717e71abe11ce736913cb3be7ab5e65236
wilitp <guilledeipola@gmail.com>
2024-05-11 11:45:11 -0300

    Casi termino el practico 4 :D

--- soluciones/lib.py
+++ soluciones/lib.py
@@ -1,20 +1,76 @@
 
 from random import random
-from typing import Any, List, Optional
+from typing import Any, Callable, List, Optional
+
+def urna(px: Callable[[Any], float], x_vals: List[Any]):
+    # 10 decimales de definicion maxima
+    MAX_DEFINITION = 10
+
+    def def_for_prob(p):
+        if p > 1:
+            raise Exception("Probabilidad > 1")
+        elif p == 0:
+            return 0
+        else:
+            return len(str(p)) - 2 # le sacamos el cero y el punto
+    
+
+    definition = min(MAX_DEFINITION, max(def_for_prob(px(x)) for x in x_vals))
 
 
-def discreta_general(p: List[float], x: List[Any]):
+    # Genero la urna
+    urn = []
+
+    for x in x_vals:
+        urn += [x] * int( 10**definition * px(x))
+    
+    U = discreta_uniforme(0, len(urn)- 1)
+
+
+    return urn[U]
+
+
+    
+
+
+def sim_esp_var(sim, n):
+    esp  = sum(sim() for _ in range(0, n))/n
+    esp2 = sum(sim()**2 for _ in range(0, n))/n
+
+    var = esp2 - esp**2
+
+    return esp, var
+
+def trans_inversa_general_optimizada(p: Callable[[Any], float], x_vals: List[Any]):
+    aux = list(zip(x_vals, (p(x) for x in x_vals)))
+
+    aux = sorted(aux, key = lambda x : x[1], reverse=True)
+
+    lookup_table = {}
+
+    for x, prob in aux:
+        lookup_table[x] = prob
+    
+    new_px = lambda x : lookup_table.get(x, 0)
+
+    new_x_vals = [x for x, _ in aux]
+
+    return trans_inversa_general(new_px, new_x_vals)
+
+def trans_inversa_general(p: Callable[[Any], float], x_vals: List[Any]):
     U = random()
 
-    i, F = 0, p[0]
+    # Empezamos a acumular la probabilidad desde el primer elemento
+
+    r = x_vals[0]
+    i, F = 0, p(r)
 
     # F es la acumulada
-
     while U >= F:
         i += 1
-        F = p[i]
+        F += p(x_vals[i])
     
-    return x[i]
+    return x_vals[i]
 
 def discreta_uniforme(m: int, n:int):
     """
@@ -66,10 +122,8 @@ def rechazo(simy, px, py, c: Optional[float] = None, x_vals: Optional[List] = No
 
         c = max(px(x) / py(x) for x in x_vals)
     
-    y = simy()
-
-
     while True:
+        y = simy()
         U = random()
 
         if U < px(y) / (c * py(y)):
@@ -79,4 +133,40 @@ def aprox_sum(a, b, term, n):
 
     return sum(term((a + (b - a) * random())) for _ in range(n)) * (1/n)* (b-a+1)
 
+
+def trans_discreta_general_infinita(px):
+    U = random()
+
+    i, F = 0, px(0)
+
+    while U >= F:
+        i += 1
+        F += px(i)
+
+    return i
+
+def trans_discreta_general_infinita_optimizada(px, mu):
+
+    F = px(0)
+
+    for j in range(1, int(mu) + 1):
+        F += px(j) 
+
+    U = random()
+    
+    if U >= F:
+        j = int(mu) + 1
+
+        while U >= F:
+            F += px(j)
+            j += 1
+        
+        return j - 1
+    else:
+        j = int(mu)
+        while U < F:
+            F -= px(j)
+            j -= 1
+
+        return j+1
 
